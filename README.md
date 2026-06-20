173bc123aeb3290f188893dc952acabf1c0a1911
JoseMoreira2026 <pedro.moreira71@gmail.com>
2026-06-20 12:33:24 +0100

    Add soma.py file

--- README.md
+++
@@ -1,2 +0,0 @@
-# soma.py
-Ler dois numeros
---
+++ soma.py
@@ -0,0 +1,11 @@
+# soma.py
+Ler dois numeros
+# Início
+# Ler os dois números
+num1 = int(input("Introduza n1: "))
+num2 = int(input("Introduza n2: "))
+# Calcular a soma
+soma = num1 + num2
+# Escrever o resultado
+print("A soma é:", soma)
+# Fim
